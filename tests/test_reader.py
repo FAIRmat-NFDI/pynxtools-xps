72a7b881540ebf12bba4e69d80ddc59e9ed4c3b0
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-04-16 09:41:47 +0200

    import function from dev_tools directly

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -21,7 +21,7 @@ from pynxtools.dataconverter.helpers import (
 from pynxtools.dataconverter.validation import validate_dict_against
 from pynxtools.dataconverter.template import Template
 from pynxtools.nexus import nexus  # noqa: E402 # noqa: E402
-from pynxtools.nexus.nxdl_utils import get_nexus_definitions_path
+from pynxtools.definitions.dev_tools.utils.nxdl_utils import get_nexus_definitions_path
 from pynxtools_xps.reader import XPSReader
 
 

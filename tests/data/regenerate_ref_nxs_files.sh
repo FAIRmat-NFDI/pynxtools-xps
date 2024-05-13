95ee978ea0ae7ebb7b7bc3ff2a9ab8d817400664
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-05-13 13:58:45 +0200

    regenerate nxs refs, add .ibw and .xy test data

--- tests/data/regenerate_ref_nxs_files.sh
+++ tests/data/regenerate_ref_nxs_files.sh
@@ -3,10 +3,27 @@ READER=xps
 NXDL=NXmpes
 
 
-function update_pro_ref {
-  cd pro/
-  echo "Update .pro (PHI depth_profiling) nexus reference"
+function update_phi_pro_ref {
+  cd phi_pro/
+  echo "Update .pro (PHI depth_profiling) NeXus reference"
   dataconverter.exe SnO2_10nm_1.pro eln_data_phi_pro.yaml --reader $READER --nxdl $NXDL --output SnO2_10nm_1.pro_ref.nxs
+  cd ..
+  echo
+}
+
+function update_phi_spe_ref {
+  cd phi_spe/
+  echo "Update .spe (PHI single spectrum) NeXus reference"
+  dataconverter SnO2_10nm.spe eln_data_phi_spe.yaml --reader $READER --nxdl $NXDL --output SnO2_10nm.spe_ref.nxs
+  cd ..
+  echo
+}
+
+function update_scienta_ibw_ref {
+  cd scienta_txt/
+  echo "Update scienta .ibw NeXus reference"
+  cd scienta_txt/
+  dataconverter Ag_000*.ibw eln_data.yaml --reader $READER --nxdl $NXDL --output Ag_ref.nxs
   cd ..
   echo
 }
@@ -20,27 +37,27 @@ function update_scienta_txt_ref {
   echo
 }
 
-function update_sle_ref {
+function update_specs_sle_ref {
   echo "Update SPECS .sle NeXus reference"
-  cd sle/
+  cd specs_sle/
   echo "Update .sle example"
   dataconverter EX439_S718_Au.sle eln_data_sle.yaml --reader $READER --nxdl $NXDL --output SPECS_SLE_ref.nxs
   cd ..
   echo
 }
 
-function update_spe_ref {
-  cd spe/
-  echo "Update .spe (PHI single spectrum) NeXus reference"
-  dataconverter SnO2_10nm.spe eln_data_phi_spe.yaml --reader $READER --nxdl $NXDL --output SnO2_10nm.spe_ref.nxs
+function update_specs_xml_ref {
+  cd specs_xml/
+  echo "Update SPECS XML NeXus reference"
+  dataconverter In-situ_PBTTT_XPS_SPECS.xml eln_data_xml.yaml --reader $READER --nxdl $NXDL --output SPECS_XML_ref.nxs
   cd ..
   echo
 }
 
-function update_vms_regular_ref {
-  cd vms_regular/
-  echo "Update regular VAMAS NeXus reference"
-  dataconverter regular.vms eln_data_vms_regular.yaml --reader $READER --nxdl $NXDL --output vms_regular_ref.nxs
+function update_specs_xy_ref {
+  cd specs_xy/
+  echo "Update SPECS XY NeXus reference"
+  dataconverter MgFe2O4_small.xy eln_data.yaml --reader $READER --nxdl $NXDL --output SPECS_XY_ref.nxs
   cd ..
   echo
 }
@@ -53,17 +70,20 @@ function update_vms_irregular_ref {
   echo
 }
 
-function update_xml_ref {
-  cd xml/
-  echo "Update SPECS XML NeXus reference"
-  dataconverter In-situ_PBTTT_XPS_SPECS.xml eln_data_xml.yaml --reader $READER --nxdl $NXDL --output SPECS_XML_ref.nxs
+function update_vms_regular_ref {
+  cd vms_regular/
+  echo "Update regular VAMAS NeXus reference"
+  dataconverter regular.vms eln_data_vms_regular.yaml --reader $READER --nxdl $NXDL --output vms_regular_ref.nxs
   cd ..
   echo
 }
 
-update_pro_ref
+update_phi_pro_ref
+update_phi_spe_ref
+# update_scienta_ibw_ref
 update_scienta_txt_ref
-update_spe_ref
-update_vms_regular_ref
+update_specs_sle_ref
+update_specs_xml_ref
+update_specs_xy_ref
 update_vms_irregular_ref
-update_xml_ref
+update_vms_regular_ref

4dd84013e650db3f25cf6eac56eb3d60345819d5
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-07-16 14:28:05 +0200

    extract datetime parsing to value_mappers

--- src/pynxtools_xps/reader_utils.py
+++ src/pynxtools_xps/reader_utils.py
@@ -82,7 +82,7 @@ class XPSMapper(ABC):
 
     def parse_file(self, file: str | Path, **kwargs):
         """
-        Parse the file using the Scienta TXT parser.
+        Parse the file using the selected parser.
 
         """
         self.file = file

37cd63a65fab27d2eec08f68c2e5833558e19ad6
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-07-25 13:26:09 +0000

    use all test cases

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -20,14 +20,14 @@ READER_CLASS = get_reader(READER_NAME)
 test_cases = [
     ("phi_spe", "Phi .spe reader"),
     ("phi_pro", "Phi .pro reader"),
-    # ("specs_sle", "SPECS .sle reader"),
-    # ("specs_xml", "SPECS .xml reader"),
-    # ("specs_xy", "SPECS .xy reader"),
-    # ("scienta_ibw", "Scienta .ibw reader"),
-    # ("scienta_txt", "Scienta .txt export reader"),
-    # ("vms_irregular", "Irregular VAMAS reader"),
-    # ("vms_regular", "Regular VAMAS reader"),
-    # ("vms_txt_export", "Vamas txt export"),
+    ("specs_sle", "SPECS .sle reader"),
+    ("specs_xml", "SPECS .xml reader"),
+    ("specs_xy", "SPECS .xy reader"),
+    ("scienta_ibw", "Scienta .ibw reader"),
+    ("scienta_txt", "Scienta .txt export reader"),
+    ("vms_irregular", "Irregular VAMAS reader"),
+    ("vms_regular", "Regular VAMAS reader"),
+    ("vms_txt_export", "Vamas txt export"),
 ]
 
 test_params = []

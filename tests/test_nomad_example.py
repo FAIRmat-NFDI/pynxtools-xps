502c7b18a09f7e379938bb5d74ac4d5ef12df980
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-14 12:29:38 +0200

    prepare nomad entrypoints incl. tests

---
+++ tests/test_nomad_example.py
@@ -0,0 +1,64 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Test for NOMAD examples in XPS reader plugin."""
+
+import pytest
+
+try:
+    from nomad.parsing.parser import ArchiveParser
+    from nomad.datamodel import EntryArchive, Context
+except ImportError:
+    pytest.skip(
+        "Skipping NOMAD example tests because nomad is not installed",
+        allow_module_level=True,
+    )
+
+from pynxtools.testing.nomad_example import (
+    get_file_parameter,
+    parse_nomad_examples,
+    example_upload_entry_point_valid,
+)
+
+from pynxtools_xps.nomad.entrypoints import xps_example
+
+
+@pytest.mark.parametrize(
+    "mainfile", get_file_parameter("src/pynxtools_xps/nomad/examples")
+)
+def test_parse_nomad_examples(mainfile):
+    """Test if NOMAD examples work."""
+    parse_nomad_examples(mainfile)
+
+
+@pytest.mark.parametrize(
+    ("entrypoint", "expected_local_path"),
+    [
+        pytest.param(
+            xps_example,
+            "examples/data/uploads/xps.zip",
+            id="xps_example",
+        ),
+    ],
+)
+def test_nomad_example_upload_entry_point_valid(entrypoint, expected_local_path):
+    """Test if NOMAD ExampleUploadEntryPoint works."""
+    example_upload_entry_point_valid(
+        entrypoint=entrypoint,
+        plugin_package="pynxtools-xps",
+        expected_local_path=expected_local_path,
+    )

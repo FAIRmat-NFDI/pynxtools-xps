95921c29127d53381d77dbf3d19d9b179d4b0d15
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-31 18:08:50 +0100

    status: able to parse XPS, read metadata

--- src/pynxtools_xps/reader.py
+++ src/pynxtools_xps/reader.py
@@ -814,7 +814,6 @@ class XPSReader(MultiFormatReader):
             return xr_data[path]
         except KeyError:
             try:
-                print(key, path)
                 return np.array(xr_data.coords[path].values)
             except KeyError:
                 return None

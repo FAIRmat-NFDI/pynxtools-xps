d05de032467cfb4b4fd51b19672dbc8fc0b966f9
fsimperl <florian.simperl@tuwien.ac.at>
2024-11-18 10:12:13 +0100

    Added version control to handle PEAK .ibw files

--- src/pynxtools_xps/scienta/scienta_data_model.py
+++ src/pynxtools_xps/scienta/scienta_data_model.py
@@ -68,3 +68,53 @@ class ScientaRegion(XpsDataclass):
     time_per_spectrum_channel: float = 0.0
     detector_mode: str = ""
     data: dict = field(default_factory=dict)
+
+
+##FLORIAN CHANGES
+@dataclass
+class ExcitationSourceEnergyInformation:
+    SourceType: str = ""
+    Energy: float = 0.0
+    TargetEnergy: float = 0.0
+    EnergyStatus: str = ""
+    EnergyStatusMessage: str = ""
+
+@dataclass
+class ScientaRegionIgorPEAK(XpsDataclass):
+    region_id: int = 0
+    Name: str = ""
+    energy_size: int = 0
+    energy_scale: str = ""
+    energy_scale_2: str = ""
+    energy_axis: np.ndarray = field(default_factory=lambda: np.zeros(0))
+    LensMode: str = ""
+    PassEnergy: float = 0.0
+    no_of_scans: int = 0
+    Energy: ExcitationSourceEnergyInformation = field(default_factory=ExcitationSourceEnergyInformation)
+    acquisition_mode: str = ""
+    energy_units: str = "kinetic"  # ???
+    center_energy: float = 0.0
+    start_energy: float = 0.0
+    stop_energy: float = 0.0
+    step_size: float = 0.0
+    DwellTime: float = 0.0
+    detector_first_x_channel: int = 0
+    detector_last_x_channel: int = 0
+    detector_first_y_channel: int = 0
+    detector_last_y_channel: int = 0
+    number_of_slices: str = ""
+    data_file: str = ""
+    SequenceName: str = ""
+    spectrum_type: str = ""
+    Instrument: str = ""
+    vendor: str = ""
+    UserName: str = ""
+    sample_name: str = ""
+    spectrum_comment: str = ""
+    Date: str = ""
+    Time: str = ""
+    time_stamp: str = ""
+    time_per_spectrum_channel: float = 0.0
+    detector_mode: str = ""
+    Data: dict = field(default_factory=dict)
+

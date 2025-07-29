27b256859b22886ad55b59b87eb98cfb64a9b088
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-07-29 16:09:02 +0200

    regenerate some reference files

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -55,7 +55,7 @@ test_params = []
 for test_case in test_cases:
     # ToDo: make tests for all supported appdefs possible
     for nxdl in NXDLS:
-        test_params += [pytest.param(nxdl, test_case[0], id=f"{test_case[1]}, {nxdl}")]
+        test_params += [pytest.param(nxdl, test_case[0], id=f"{test_case[1]}-{nxdl}")]
 
 
 @pytest.mark.parametrize(

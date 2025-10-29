a36b98e16a1bf4a4538de43f577d1ef879d16456
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-10-29 22:57:09 +0100

    bring in changes from pynxtools-plugin-template

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -15,13 +15,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""
-Basic example based test for the XPS reader
-"""
+"""Tests for the pynxtools reader plugin."""
 
 import json
 import os
-from typing import Literal
+from typing import Any, Literal
 
 import numpy as np
 import pytest
@@ -35,45 +33,61 @@ READER_CLASS = get_reader(READER_NAME)
 # TODO: make tests for all supported application definitions possible
 NXDLS = ["NXxps"]  # READER_CLASS.supported_nxdls
 
-test_cases = [
-    ("phi_spe", "phi-spe-reader", {}),
-    ("phi_pro", "phi-pro-reader", {}),
-    ("specs_sle", "specs-sle-reader", {}),
-    ("specs_xml", "specs-xml-reader", {}),
-    ("specs_xy", "specs-xy-reader", {}),
+# Define lines/sections to be ignored in _all_ test cases
+ignore_lines_all_tests: list = []
+ignore_sections_all_tests: dict = {}
+
+# Test cases should be [("folder", ignore_lines, ignore_sections, "test-id")]
+test_cases: list[tuple[str, list[Any], dict[Any, Any], str]] = [
+    (
+        "phi_spe",
+        [],
+        {},
+        "phi-spe-reader",
+    ),
+    ("phi_pro", [], {}, "phi-pro-reader"),
+    ("specs_sle", [], {}, "specs-sle-reader"),
+    ("specs_xml", [], {}, "specs-xml-reader"),
+    ("specs_xy", [], {}, "specs-xy-reader"),
     (
         "scienta_ibw",
-        "scienta-ibw-reader",
+        [],
         {"FIELD (//Ag__002__VB/start_time)": ["DEBUG - value:"]},
+        "scienta-ibw-reader",
     ),
     (
         "scienta_txt",
-        "scienta-txt-reader",
+        [],
         {"FIELD (//Ag__002__VB/start_time)": ["DEBUG - value:"]},
+        "scienta-txt-reader",
     ),
-    ("vms_analysis", "vms-reader-with-data-analysis", {}),
-    ("vms_irregular", "irregular-vms-reader", {}),
-    ("vms_regular", "regular-vms-reader", {}),
-    ("vms_txt_export", "vms-txt-export-reader", {}),
+    ("vms_analysis", [], {}, "vms-reader-with-data-analysis"),
+    ("vms_irregular", [], {}, "irregular-vms-reader"),
+    ("vms_regular", [], {}, "regular-vms-reader"),
+    ("vms_txt_export", [], {}, "vms-txt-export-reader"),
 ]
 
 test_params = []
-
 for test_case in test_cases:
-    # ToDo: make tests for all supported appdefs possible
     for nxdl in NXDLS:
         test_params += [
             pytest.param(
-                nxdl, test_case[0], test_case[2], id=f"{test_case[1]}-{nxdl.lower()}"
+                nxdl,
+                test_case[0],
+                test_case[1],
+                test_case[2],
+                id=f"{test_case[3]}-{nxdl.lower()}",
             )
         ]
 
 
 @pytest.mark.parametrize(
-    "nxdl, sub_reader_data_dir, ignore_sections",
+    "nxdl, sub_reader_data_dir, ignore_lines, ignore_sections",
     test_params,
 )
-def test_nexus_conversion(nxdl, sub_reader_data_dir, ignore_sections, tmp_path, caplog):
+def test_nexus_conversion(
+    nxdl, sub_reader_data_dir, ignore_lines, ignore_sections, tmp_path, caplog
+):
     """
     Test XPS reader
 
@@ -81,13 +95,15 @@ def test_nexus_conversion(nxdl, sub_reader_data_dir, ignore_sections, tmp_path, 
     ----------
     nxdl : str
         Name of the NXDL application definition that is to be tested by
-        this reader plugin (e.g. NXxps, NXmpes, etc)..
+        this reader plugin.
     sub_reader_data_dir : str
         Test data directory that contains all the files required for running the data
         conversion through one of the sub-readers. All of these data dirs
         are placed within tests/data/...
+    ignore_lines: Dict[str, List[str]]
+        Lines within the log files to ignore.
     ignore_sections: Dict[str, List[str]]
-        Subsections of the log file to ignore.
+        Subsections of the log files to ignore.
     tmp_path : pathlib.PosixPath
         Pytest fixture variable, used to clean up the files generated during
         the test.
@@ -101,12 +117,13 @@ def test_nexus_conversion(nxdl, sub_reader_data_dir, ignore_sections, tmp_path, 
 
     """
     caplog.clear()
-    reader = READER_CLASS
-    assert callable(reader.read)
 
     files_or_dir = os.path.join(
         *[os.path.dirname(__file__), "data", sub_reader_data_dir]
     )
+
+    ignore_lines: list[str] = ignore_lines_all_tests + ignore_lines
+    ignore_sections: dict[str, list[str]] = ignore_sections_all_tests | ignore_sections
 
     test = ReaderTest(
         nxdl=nxdl,
@@ -116,7 +133,9 @@ def test_nexus_conversion(nxdl, sub_reader_data_dir, ignore_sections, tmp_path, 
         caplog=caplog,
     )
     test.convert_to_nexus(caplog_level="WARNING", ignore_undocumented=True)
-    test.check_reproducibility_of_nexus(ignore_sections=ignore_sections)
+    test.check_reproducibility_of_nexus(
+        ignore_lines=ignore_lines, ignore_sections=ignore_sections
+    )
 
 
 def read_comment_file(filepath: str):

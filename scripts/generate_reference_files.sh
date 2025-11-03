b4e5db3f3395c1529433a3a90f1d9cdcba5cab98
Lukas Pielsticker <you@example.com>
2025-11-03 14:48:25 +0100

    update from cruft

--- scripts/generate_reference_files.sh
+++ scripts/generate_reference_files.sh
@@ -3,6 +3,9 @@ function update_ref_file {
   local FOLDER=$1
   local NXDL=$2
   cd $FOLDER
+  if [[ "$FOLDER" == "." ]]; then
+    FOLDER="test"
+  fi
   echo "Update $FOLDER reference file for $NXDL"
   files=$(find . -type f \( ! -name "*.log" -a ! -name "*.nxs" \))
   dataconverter ${files[@]} --reader $READER --nxdl $NXDL --ignore-undocumented --output "${FOLDER}_ref.nxs" # &> ref_output.txt
@@ -31,7 +34,6 @@ nxdls=(
 
 project_dir=$(dirname $(dirname $(realpath $0)))
 cd $project_dir/tests/data
-
 
 for folder in "${folders[@]}"; do
   for nxdl in "${nxdls[@]}"; do
